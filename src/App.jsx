297598d9e27cdc22ff83866a3e8241ad2adf9081
Ana Mari <anitamari.1979@gmail.com>
2023-06-12 11:13:36 +0200

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+//import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+//vamos a crear el useEffect para hacer la bolita o una animación
+//y useState para activarlo o desactivarlo
+
+function App() { //1. creamos el useState y useEffect
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 }) //6. para guardar la posición. Una buena práctica es inicializarlo con el tipo de dato que vamos a usar siempre, o null si no tienes ese dato
+  useEffect(() => {
+    console.log("efecto", { enabled }) //en la consola se ve true, false, true...
+    
+    const handleMove = (event) => { //4. para controlar cuando se hace este efecto
+      const { clientX, clientY } = event // (para saber la posición)
+      console.log("handleMove", { clientX, clientY }) //te dice la posición del puntero en la pantalla
+      setPosition({ x: clientX, y: clientY }) //8. para actualizar la posición
+    }
+
+    if (enabled) { //5. solo me suscribo al evento si es true. No se podría meter el useEffect dentro del if
+      window.addEventListener("pointermove", handleMove) 
+    }
+//9. Limpiar el useEffect para que deje de estar activo una vez que le damos al botón de desactivar:
+//Cleanup cuando el componente se desmonta y cuando cambian las dependencias, antes de ejecutar 
+  return () => {
+    console.log("cleanup")
+      window.removeEventListener("pointermove", handleMove) //para limpiar la suscripción anterior
+ }
+ //si pusiera en la consola getEvenListeners(window) me aparecería cuántas veces me suscribo al evento (truco). Usarlo para ver si se limpia bien el evento. Solo en Chromium
+
+  }, [enabled]) //le decimos que se tiene que ejecutar cada vez que se actualice el valor del enabled
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>    
+      <h3>Mouse Follower</h3>
+      {/* 3. hacer la bolita */}
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        // transform: "translate(0px, 0px)". 7. Abajo:
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+
+
+      {/* 2. con el botón vamos a cambiar el estado. Cada vez que lo hacemos se renderiza el efecto */}
+      <button onClick={() => setEnabled(!enabled)}> 
+        {enabled ? "Desactivar" : "Activar"} seguir puntero</button>
+    </main>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+//cuando empieza esto hace el effecto, cleanup y effecto para comprobar que funciona bien. Si se ejecutara dos veces es porque hay algo que no está bien.
+//el StrictMode solo se usa en desarrollo, no en producción. Es para ayudar al desarrollador, en producción no lo ven. Se puede quitar y no pasa nada.
+//con las React Development Tools puedes ver si estás usando el modo desarrollo o producción (icono rojo dcha barra google arriba)
+//en components se pueden ver las props, los hooks en la app, etc. Puedes replicar estados, props... manualmente para ver qué pasa (si está en modo desarrollo y no en producción)
